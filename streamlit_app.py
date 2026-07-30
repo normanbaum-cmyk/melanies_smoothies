b551c555e1abdd0067b1a3afe63ccf7fa9567a8b
normanbaum-cmyk <norman.baum@googlemail.com>
2026-07-30 12:00:18 +0200

    Update streamlit_app.py
    Smoothiefront nutrition information added

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,8 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
     
-
+# New Section to display smoothiefroot nutrition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 

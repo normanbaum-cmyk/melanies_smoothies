212c1e9687a1dd4014908398971245265e9e400d
normanbaum-cmyk <norman.baum@googlemail.com>
2026-07-30 13:21:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ pd_df=my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
-    , my_dataframe
+    , pd_df # my_dataframe
     , max_selections=5
     
 )
